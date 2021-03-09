753f3f8f14349ca002e31fba46e34d92c6fee335
Michael Miller <61704000+gitty-miller@users.noreply.github.com>
2021-03-09 11:52:13 -0500

    Clickable objects in now

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+                Debug.Log("Item here!");
+            else
+                Debug.Log("No item here");
+        }
+    }
+}
